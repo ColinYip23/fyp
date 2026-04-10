1aedb59b6b6590976aac3b42481d4db48d60c5ca
Toodliess <sbil0006@student.monash.edu>
2026-04-11 03:10:48 +0800

    function write_status created.

--- app/backend/app.py
+++ app/backend/app.py
@@ -16,3 +16,18 @@ BASE_DIR.mkdir(exist_ok=True)
 
 # 5 MB
 MAX_FILE_SIZE = 5 * 1024 * 1024
+
+# Will be used to store and show the current status of each run
+def write_status(run_dir: Path, phase: str, message: str, done: int, total: int):
+    status_path = run_dir / "status.json"
+    with open(status_path, "w") as f:
+        json.dump({
+            "phase": phase,
+            "message": message,
+            "progress": {
+                "done": done,
+                "total": total
+            }
+        }, f)
+
+        
